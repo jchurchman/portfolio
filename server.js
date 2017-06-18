e77f1a954d1aec128f211c1c712a66a5e0916d2a
Joe Churchman <churchman.joe@gmail.com>
2017-06-18 12:39:44 -0700

    Added server routing to fetch data from github

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const express = require('express');
+const pg = require('pg');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const dotenv = require('dontenv').config();
+const requestProxy = require('express-request-proxy');
+const conString = process.env.DATABASE_URL;
+
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -10,6 +20,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 app.get('./data/abouts.json', (req, res) => res.sendFile('abouts.json', { root: './public' }));
+
+function proxyGitHub( request, response ) {
+    console.log( 'Routing GitHub request for', requestAnimationFrame.params[0] );
+    ( requestProxy({
+        url: `https://api.github.com/${request.params[0]}`,
+        headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+    }))( request, response );
+}
+
+app.get('/github/*', proxyGitHub);
+
 
 app.listen(PORT, function () {
     console.log(`On port ${PORT}? Yup!`);
