e931c5764c046a329c8b3ef749c0d87b1eb6196f
Joe Churchman <churchman.joe@gmail.com>
2017-11-02 10:11:27 -0700

    basic nav list written

---
+++ src/Navigate/Navigate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Navigate() {
+    return (
+        <ul className="NavList">
+            <li className="Link"><NavLink to="/">Home</NavLink></li>
+            <li className="Link"><NavLink to="/about">About</NavLink></li>
+            <li className="Link"><NavLink to="/experience">Experience</NavLink></li>
+            <li className="Link"><NavLink to="/projects">projects</NavLink></li>
+        </ul>
+    )
+}
