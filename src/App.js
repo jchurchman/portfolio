076cdf71cc85c5dbb5edd91cefdac2e112126794
Joe Churchman <churchman.joe@gmail.com>
2017-11-02 13:49:25 -0700

    fixed initial rendering issues

--- src/App.js
+++ src/App.js
@@ -6,23 +6,27 @@ import {
   Redirect,
   Route
 } from 'react-router-dom';
+import Home from './Home/Home';
 
 class App extends Component {
 
   render() {
     return (
       <Router>
+        <div>
           <header className="App-header">
             <h1 className="App-title">Joe Churchman</h1>
             <Navigate />
           </header>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/projects" component={Projects} />
+            {/* <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} /> */}
           </Switch>
           <footer className="App-footer">
+
           </footer>
+        </div>
       </Router>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './root-reducer';
 
